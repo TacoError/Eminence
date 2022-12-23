12191069fc053f760475f983a32d16be7ffcbf22
Taylor <92060690+TacoError@users.noreply.github.com>
2022-12-23 19:21:01 +0000

    hero selection (beginning) and some other frontend changes.

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,3 +1,7 @@
+let loginNodeCopy = null;
+
 function play() {
-	
+	loginNodeCopy = document.getElementById("login").cloneNode();
+	document.getElementById("login").remove();
+	document.getElementById("heroes").style.visibility = "visible";
 }
--- client/scripts/socket.js
+++ client/scripts/socket.js
@@ -1,10 +1,38 @@
 const socket = io();
 
+function undent(string) {
+	return string.replace(/  +/g, "");
+}
+
 socket.on("connect", () => {
 	document.getElementById("connecting").remove();
 	document.getElementById("content").style.visibility = "visible";
 });
 
 socket.on("heroes", (list) => {
-	
+	for (const key of Object.keys(list)) {
+		const div = document.createElement("div");
+		div.classList.add("heroDescription");
+		
+		const data = list[key];
+
+		const name = document.createElement("h2");
+		name.innerHTML = key;
+		const description = document.createElement("p1");
+
+		const powerOneDescription = data[2] == null ? "No Power One" : data[2];
+		const powerTwoDescription = data[3] == null ? "No Power Two" : data[3];
+		
+		description.innerHTML = undent(`
+	  		Hero Name: ${data[0]}<br>
+			Hero Description: ${data[1]}<br>
+	  		Power One Description: ${powerOneDescription}<br>
+			Power Two Description: ${powerTwoDescription}
+  		`);
+
+		div.appendChild(name);
+		div.appendChild(description);
+
+		document.getElementById("heroes").appendChild(div);
+	}
 });
